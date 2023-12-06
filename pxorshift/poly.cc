838481ff25ac7351207e1f3e8c628fed1cd7216c
paddison <patrickbaumann579@gmail.com>
2023-12-06 11:27:10 +0100

    refactor sliding window algorithm

--- pxorshift/poly.cc
+++ pxorshift/poly.cc
@@ -2,12 +2,14 @@
 #include "NTL/tools.h"
 #include "NTL/vec_GF2.h"
 #include "gray/gray.h"
+#include <bitset>
 #include <cstdlib>
 #include <iostream>
 #include <sstream>
+#include <cassert>
 
 #define SEED 123234345ull
-#define Q 8
+#define Q 10
 
 const size_t STATE_SIZE = 64;
 const uint64_t a = 698769069ull;
@@ -69,24 +71,6 @@ uint32_t jump(uint64_t seed) {
     return horner(seed);    
 }
 
-int horner_man(size_t deg, int poly[deg], int x) {
-    size_t i = 0;
-    while (poly[i] == 0) ++i;
-
-    int ret = x * poly[i];
-    ++i;
-
-    printf("%d\n", ret);
-    for (; i < deg - 1; ++i) {
-        if (poly[i] != 0) ret += poly[i];
-        ret *= x; 
-        printf("%d\n", ret);
-    }
-    if (poly[deg - 1] != 0) ret += poly[deg - 1];
-    printf("%d\n", ret);
-    return ret;
-}
-
 uint16_t gray[1 << Q];
 
 uint64_t h[1 << Q];
@@ -94,178 +78,137 @@ uint16_t t[1 << Q];
 
 void print_gray(size_t i) {
     uint64_t mask = 1 << (Q - 1);
-    while (mask > 0) {
-        std::cout << ((gray[i] & mask) != 0 ? 1 : 0) << " ";
-        mask /= 2;
-    }
-    std::cout << std::endl;
+    std::bitset<Q> bits(i);
+    std::cout << bits << std::endl;
 }
 
-void gray_enumeration() {
-    size_t q = 4;
-    uint64_t A_j[Q + 1];
-    uint64_t seed = 3;
-    uint64_t seed_tmp = seed;
-    gray_code(Q, gray);
+// represents the current state, initialized with the seed
+struct State {
+    uint64_t s;
+};
 
-    // precompute A^j * x 
+// precomputes A^0x..A^qx which are the first q state transitions
+// (equivalent to repeated calls to step()
+uint64_t* precompute_A(size_t q, uint64_t A[q + 1], const State s) {
+    State state_tmp = s;
     for (size_t i = 0; i < Q + 1; ++i) { 
-        A_j[i] = seed_tmp;
-        seed_tmp <<= 2;
+        A[i] = state_tmp.s;
+        //std::cout << A_j[i] << " " << seed_tmp << std::endl;
+        state_tmp.s = step(state_tmp.s);
     }
+    
+    return A;
+}
 
-    for (size_t i = 0; i < Q + 1; ++i) {
-        std::cout << A_j[i] << std::endl; 
-    }
-    // do all the rest
-    // polynomials in h always contain z^q, so 
-    // each of the 2^q polynomials h contains A^j * x
-    h[0] = A_j[Q];
+uint64_t* compute_decomposition_polys(size_t q, uint64_t h[1 << q], 
+                                      uint64_t A[q + 1]) {
+    // A[Q] is the polynomial component which contains z^q,
+    // which is always included, and thus used to initialize
+    // the decomposition polys
+    h[0] = A[Q];
     
     for (size_t i = 1; i < (1 << Q); ++i) {
+        // find out which bit flips
         uint64_t diff = gray[i] ^ gray[i - 1];
         size_t set_bit = 0;
+
         while (diff > 0) {
             diff >>= 1;
             ++set_bit;
         }
-        print_gray(i);
 
-        h[gray[i]] = h[gray[i - 1]] ^ A_j[set_bit - 1];
-        std::bitset<16> bin(h[gray[i]]);
-        std::cout << "h(A)*x: " << bin << " diff: " << set_bit - 1 << std::endl;
-
+        h[gray[i]] = h[gray[i - 1]] ^ A[set_bit - 1];
     }
 
+    return h;
+}
+
+inline size_t determine_gray_enumeration(size_t q, size_t i, NTL::GF2X poly);
+
+uint64_t horner_sliding_window(size_t q, uint64_t h[1 << q]) {
+    // use horners method with sliding window
+    uint64_t state = 0;
+    int i = NTL::deg(g); 
+    std::cout << "poly_length: " << i << std::endl;
+    int dj = 0, dj_next = 0, dm = -1;
+
+    // go to first non zero coefficient
+    // NTL polynomials should always start with the first non zero component
+    assert(NTL::coeff(g, i) == 1);
+    dj = i;
+
+    // determine which gray enumeration to use
     
+    //; h1(A) * x, first component in horner's method
+    size_t gray = determine_gray_enumeration(Q, i, g);
+    state = h[gray];//[determine_gray_enumeration(Q, i, g)];
+
+    i -= (Q + 1);
+
+    for (; i >= Q; --i) {
+        if (g[i] == 1) {
+            std::cout << "i: " << i << std::endl;
+            dj_next = i;
+            // step forward dj - dj_next steps, which is the same as
+            // multiplying with A^(dj - dj_next)
+            for (size_t j = 0; j < dj - dj_next; ++j) state = step(state);
+
+            // find out the gray_enumeration of the current decomposition
+            // polynomials and calculate cur_state + h_i(A)x
+            size_t gray = determine_gray_enumeration(Q, i, g);
+            state ^= h[gray];//determine_gray_enumeration(Q, i, g)];
+
+            i -= Q;
+            dj = dj_next;
+        }
+    }
+
+    // calculate h_m+1 and do the last part of the decomposition 
+    // which is multiplying the current state with A^dm
+    // xoring with h_m+1(A)x and adding A^qx
+    dm = dj - Q;
+
+    for (size_t j = 0; j < dm; ++j) state = step(state);
+
+    size_t g2 = determine_gray_enumeration(i + 1, i + 1, g);
+    return state ^ h[g2] ^ h[0];
+}
+
+inline size_t determine_gray_enumeration(size_t q, size_t i, NTL::GF2X poly) {
+    size_t gray_enumeration = 0;
+
+    for (size_t j = 0; j < q; ++j) {
+        gray_enumeration = (gray_enumeration << 1) ^ NTL::rep(NTL::coeff(g, i - j - 1));
+    }
+    
+    return gray_enumeration;
 }
 
 uint64_t sliding_window(uint64_t seed) {
     // in the paper, the seed is denoted as 'x'
 
-    uint64_t A_j[Q + 1]; // = A^j * x
+    uint64_t A[Q + 1]; // = A^j * x
     uint64_t seed_tmp = seed;
+    State s = (State) { .s = seed };
     
-    /*
-    g = NTL::GF2X();
-
-    NTL::SetCoeff(g, 18, 0);
-    NTL::SetCoeff(g, 17, 0);
-    NTL::SetCoeff(g, 16, 0);
-    NTL::SetCoeff(g, 15, 1);
-    NTL::SetCoeff(g, 14, 1);
-    NTL::SetCoeff(g, 13, 1);
-    NTL::SetCoeff(g, 12, 1);
-    NTL::SetCoeff(g, 11, 0);
-    NTL::SetCoeff(g, 10, 1);
-    NTL::SetCoeff(g, 9, 0);
-    NTL::SetCoeff(g, 8, 0);
-    NTL::SetCoeff(g, 7, 0);
-    NTL::SetCoeff(g, 6, 1);
-    NTL::SetCoeff(g, 5, 1);
-    NTL::SetCoeff(g, 4, 1);
-    NTL::SetCoeff(g, 3, 0);
-    NTL::SetCoeff(g, 2, 0);
-    NTL::SetCoeff(g, 1, 0);
-    NTL::SetCoeff(g, 0, 1);
-
-    std::cout << g << std::endl;
-    std::cout << NTL::deg(g) << std::endl;
-    
-    */
+    // do gray code enumeration
     gray_code(Q, gray);
 
     // precompute A^j * x 
-    for (size_t i = 0; i < Q + 1; ++i) { 
-        A_j[i] = seed_tmp;
-        //std::cout << A_j[i] << " " << seed_tmp << std::endl;
-        seed_tmp = step(seed_tmp);
-    }
+    precompute_A(Q, A, s);
+
     // do all the rest
     // polynomials in h always contain z^q, so 
     // each of the 2^q polynomials h contains A^j * x
-    h[0] = A_j[Q];
-    
-    for (size_t i = 1; i < (1 << Q); ++i) {
-        uint64_t diff = gray[i] ^ gray[i - 1];
-        //std::cout << i << std::endl;
-        size_t set_bit = 0;
-        while (diff > 0) {
-            diff >>= 1;
-            ++set_bit;
-        }
-        h[gray[i]] = h[gray[i - 1]] ^ A_j[set_bit - 1];
-    }
+    compute_decomposition_polys(Q, h, A);
 
-    // use horners method with sliding window
-    uint64_t state = 0;
-    int deg = NTL::deg(g);
-    int i = deg; 
-    size_t coeffs_id = 0;
-    int d1 = 0;
-    int d2;
-
-    // go to first non zero coefficient
-    while (g[i] == 0) --i;
-    d1 = i;
-
-    // determine which gray enumeration to use
-    for (size_t j = 0; j < Q; ++j) {
-        coeffs_id = (coeffs_id << 1) ^ NTL::rep(NTL::coeff(g, i - j - 1));
-    }
-
-    // h1(A) * x
-
-#define w 10
-    std::bitset<w> gray(coeffs_id);
-    state = h[coeffs_id];
-    //std::cout << "h1: " << gray << ", d1: " << d1 << ", d2: " << d2 << std::endl;
-    i -= (Q + 1);
-
-    for (; i >= Q; --i) {
-        if (g[i] == 1) {
-            d2 = i;
-            // std::cout << "A^" << d1 - d2 << std::endl;
-            for (size_t j = 0; j < d1 - d2; ++j) state = step(state);
-
-            coeffs_id = 0;  
-
-            for (size_t j = 0; j < Q; ++j) {
-                coeffs_id = (coeffs_id << 1) ^ NTL::rep(NTL::coeff(g, i - j - 1));
-            }
-
-            std::bitset<w> gray(coeffs_id);
-            //std::cout << "xor with: " << gray << ", d1: " << d1 << ", d2: " << d2 << std::endl;
-            state ^= h[coeffs_id];
-            i -= Q;
-            d1 = d2;
-        }
-    }
-    coeffs_id = 0;
-    int dm = i + 1;
-
-    //std::cout << "A^" << dm << std::endl;
-    for (size_t j = 0; j < dm; ++j) state = step(state);
-    while (i > -1) {
-        if (NTL::coeff(g, i) == 1) { 
-            coeffs_id |= (NTL::rep(NTL::coeff(g, i)) & 1) << i;
-        }
-        --i;
-    }
-    std::bitset<w> gray2(coeffs_id);
-    state ^= h[coeffs_id];
-
-    //std::cout << "dm: " << dm << " hm+1: " << gray2 << std::endl;
-    
-    state ^= h[0];
-
-    return state;
+    return horner_sliding_window(Q, h);
 }
 
 
 // calculate jump polynomial by evaluating with horners method
 int main(void) {
-    size_t J = 123456;
+    size_t J = 123456000;
     uint64_t seed = SEED;
     init_min_poly(seed);
     init_jump_poly(J);
