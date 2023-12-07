3ac689bd47451d354d19d33b28feb462437647a8
paddison <patrickbaumann579@gmail.com>
2023-12-07 15:59:46 +0100

    refactor state of rng and step function into separate file

---
+++ pxorshift/xorshift64.h
@@ -0,0 +1,21 @@
+#ifndef RNG_STATE_64_H
+#define RNG_STATE_64_H
+
+#include <stdlib.h>
+/**
+ * Represents the current state of the rng, e.g. the current random number
+ */
+typedef struct StateRng64 StateRng64;
+typedef struct StateRng64 StateRng;
+
+struct StateRng64 {
+    uint64_t s; 
+};
+
+StateRng64* state_rng_init(StateRng64* rng, void* seed);
+StateRng64* state_rng_copy(StateRng64* dest, const StateRng64* source);
+StateRng64* state_rng_add(StateRng64* lhs, const StateRng64* rhs);
+StateRng64* step(StateRng64* rng); 
+void set_params(uint64_t a, uint64_t b, uint64_t c);
+
+#endif
