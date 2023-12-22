ed88c7260d9410c96a0395cc589a316b9a26315f
paddison <patrickbaumann579@gmail.com>
2023-12-22 18:32:14 +0100

    fix memory leak in jump_ahead.c

--- f2lin/src/jump_ahead.c
+++ f2lin/src/jump_ahead.c
@@ -13,20 +13,41 @@
  /----------------------------------------------------*/
 
 // functions used for initialization
-static GF2X* init_jump_poly(const GF2X* min_poly, const size_t jump_size);
-static void init_decomp_poly();
-static GF2X* load_min_poly();
+static 
+GF2X* init_jump_poly(const GF2X* min_poly, const size_t jump_size);
+static 
+void init_decomp_poly();
+static 
+GF2X* load_min_poly();
 
 // verification
-static void verify_config(F2LinConfig* cfg);
+static 
+void verify_config(F2LinConfig* cfg);
 
 // functions used for implementing the jump algorithm
-static void init_sliding_window(const int Q, F2LinRngGeneric* h[1 << Q], F2LinRngGeneric* rng);
-static void precompute_A(const int Q, F2LinRngGeneric* A[Q + 1], const F2LinRngGeneric* rng);
-static void compute_decomposition_polys(const int Q, F2LinRngGeneric* h[1 << Q], const F2LinRngGeneric* A[Q + 1]);
-static F2LinRngGeneric* horner(F2LinRngGeneric* rng, const GF2X* jump_poly);
-static F2LinRngGeneric* horner_sliding_window(const int Q, F2LinRngGeneric* rng, const GF2X* jump_poly, const F2LinRngGeneric* h[1 << Q]);
-static F2LinRngGeneric* horner_sliding_window_decomp(const int Q, F2LinRngGeneric* rng, const F2LinPolyDecomp* jump_poly, const F2LinRngGeneric* h[1 << Q]);
+static 
+void init_sliding_window(const int Q, F2LinRngGeneric* h[1 << Q], 
+                         F2LinRngGeneric* rng);
+
+static 
+void precompute_A(const int Q, F2LinRngGeneric* A[Q + 1], 
+                  const F2LinRngGeneric* rng);
+
+static 
+void compute_decomposition_polys(const int Q, F2LinRngGeneric* h[1 << Q], 
+                                 const F2LinRngGeneric* A[Q + 1]);
+
+static 
+F2LinRngGeneric* horner(F2LinRngGeneric* rng, const GF2X* jump_poly);
+
+static 
+F2LinRngGeneric* sliding_window(const int Q, F2LinRngGeneric* rng, 
+                                const GF2X* jump_poly, const F2LinRngGeneric* h[1 << Q]);
+
+static 
+F2LinRngGeneric* sliding_window_decomp(const int Q, F2LinRngGeneric* rng, 
+                                       const F2LinPolyDecomp* jump_poly, 
+                                       const F2LinRngGeneric* h[1 << Q]);
 
 
 /*------------------------------------------------------ 
@@ -35,7 +56,7 @@ static F2LinRngGeneric* horner_sliding_window_decomp(const int Q, F2LinRngGeneri
 
 F2LinJump* f2lin_jump_ahead_init(size_t jump_size, F2LinConfig* cfg) {
     // load the minimal polynomial from the header
-    const GF2X* min_poly = load_min_poly();
+    GF2X* min_poly = load_min_poly();
     F2LinJump* jump_params = calloc(1, sizeof(F2LinJump));
     F2LinConfig def = { .q = Q_DEFAULT, .algorithm = ALGORITHM_DEFAULT };
 
@@ -47,10 +68,18 @@ F2LinJump* f2lin_jump_ahead_init(size_t jump_size, F2LinConfig* cfg) {
     jump_params->q = cfg->q;
     jump_params->algorithm = cfg->algorithm;
     jump_params->jump_poly = init_jump_poly(min_poly, jump_size);
-    jump_params->h = malloc(sizeof(F2LinRngGeneric*) * (1 << jump_params->q));
 
-    // initialize jump and decomposition polynomials
-    jump_params->decomp_poly = f2lin_poly_decomp_init_from_gf2x(jump_params->jump_poly, jump_params->q);
+    if (cfg->algorithm == HORNER) {
+        jump_params->h = 0;
+        jump_params->decomp_poly = 0;
+    } else {
+        jump_params->h = calloc(sizeof(F2LinRngGeneric*), (1 << jump_params->q));
+        for (size_t i = 0; i < (1 << jump_params->q); ++i) {
+            jump_params->h[i] = f2lin_rng_generic_init();
+        }
+        jump_params->decomp_poly = f2lin_poly_decomp_init_from_gf2x(jump_params->jump_poly, jump_params->q);
+    }
+
     GF2X_zero_destroy(min_poly);
     return jump_params;
 }
@@ -62,12 +91,12 @@ F2LinRngGeneric* f2lin_jump_ahead_jump(F2LinJump* jump_params, F2LinRngGeneric* 
     init_sliding_window(jump_params->q, jump_params->h, rng);
 
     if (jump_params->algorithm == SLIDING_WINDOW) {
-        return horner_sliding_window(jump_params->q, 
+        return sliding_window(jump_params->q, 
                                      rng, 
                                      jump_params->jump_poly, 
                                      jump_params->h); 
     } else { 
-        return horner_sliding_window_decomp(jump_params->q, 
+        return sliding_window_decomp(jump_params->q, 
                                             rng, 
                                             jump_params->decomp_poly, 
                                             jump_params->h);
@@ -76,12 +105,19 @@ F2LinRngGeneric* f2lin_jump_ahead_jump(F2LinJump* jump_params, F2LinRngGeneric* 
 
 void f2lin_jump_ahead_destroy(F2LinJump* jump_params) {
     GF2X_zero_destroy(jump_params->jump_poly);
-    f2lin_poly_decomp_destroy(jump_params->decomp_poly);
-    for (size_t i = 0; i < 1 << jump_params->q; ++i) {
-        f2lin_rng_generic_destroy(jump_params->h[i]);
+    jump_params->jump_poly = 0;
+
+    if (jump_params->algorithm != HORNER) {
+        f2lin_poly_decomp_destroy(jump_params->decomp_poly);
+        jump_params->decomp_poly = 0;
+
+        for (size_t i = 0; i < (1 << jump_params->q); ++i) {
+            f2lin_rng_generic_destroy(jump_params->h[i]);
+        }
+
+        free(jump_params->h);
+        jump_params->h = 0;
     }
-    free(jump_params->h);
-    free(jump_params->decomp_poly);
     free(jump_params);
 }
 
@@ -89,7 +125,8 @@ void f2lin_jump_ahead_destroy(F2LinJump* jump_params) {
  * Internal Implementations                            |
  /----------------------------------------------------*/
 
-static GF2X* init_jump_poly(const GF2X* min_poly, const size_t jump_size) {
+static 
+GF2X* init_jump_poly(const GF2X* min_poly, const size_t jump_size) {
     GF2X* jump_poly = GF2X_zero_init();
     GF2X* x = GF2X_zero_init();
     GF2X_SetCoeff(x, 1, 1);
@@ -98,11 +135,13 @@ static GF2X* init_jump_poly(const GF2X* min_poly, const size_t jump_size) {
     GF2XModulus_build(minimal_poly_mod, min_poly);
     GF2X_PowerMod(jump_poly, x, jump_size, minimal_poly_mod);
     
-    //GF2X_print(jump_poly);
+    GF2X_zero_destroy(x);
+    GF2XModulus_destroy(minimal_poly_mod);
     return jump_poly;
 }
 
-static GF2X* load_min_poly() {
+static 
+GF2X* load_min_poly() {
     GF2X* min_poly = GF2X_zero_init();
     char* min_poly_string = f2lin_rng_generic_min_poly();
     for (size_t i = 0; min_poly_string[i] != 0; ++i) {
@@ -111,14 +150,16 @@ static GF2X* load_min_poly() {
     return min_poly;
 }
 
-static void verify_config(F2LinConfig* cfg) {
+static 
+void verify_config(F2LinConfig* cfg) {
     if (cfg->q > Q_MAX || cfg->q == 0)  {
         fprintf(stderr, "Invalid value for Q: %d, defaulting to 4", cfg->q);
         cfg->q = Q_DEFAULT;
     }
 }
 
-static void init_sliding_window(const int Q, F2LinRngGeneric* h[1 << Q], F2LinRngGeneric* rng) {
+static 
+void init_sliding_window(const int Q, F2LinRngGeneric* h[1 << Q], F2LinRngGeneric* rng) {
     // in the paper, the seed is denoted as 'x'
     F2LinRngGeneric* A[Q + 1]; // = A^j * x
 
@@ -132,15 +173,14 @@ static void init_sliding_window(const int Q, F2LinRngGeneric* h[1 << Q], F2LinRn
     for (size_t i = 0; i < Q + 1; ++i) {
         f2lin_rng_generic_destroy(A[i]);
     }
-
 }
 
 // precomputes A^0x..A^qx which are the first q state transitions
 // (equivalent to repeated calls to step()
-static void precompute_A(const int Q, F2LinRngGeneric* A[Q + 1], const F2LinRngGeneric* rng) {
+static 
+void precompute_A(const int Q, F2LinRngGeneric* A[Q + 1], const F2LinRngGeneric* rng) {
     F2LinRngGeneric* tmp = f2lin_rng_generic_init_zero();// = calloc(1, sizeof(*rng)); 
     f2lin_rng_generic_copy(tmp, rng);
-
 
     for (size_t i = 0; i < Q + 1; ++i) { 
         A[i] = f2lin_rng_generic_init_zero();
@@ -152,11 +192,10 @@ static void precompute_A(const int Q, F2LinRngGeneric* A[Q + 1], const F2LinRngG
 
 static 
 void compute_decomposition_polys(const int Q, F2LinRngGeneric* h[1 << Q], 
-                                      const F2LinRngGeneric* A[Q + 1]) {
+                                 const F2LinRngGeneric* A[Q + 1]) {
     // A[Q] is the polynomial component which contains z^q,
     // which is always included, and thus used to initialize
     // the decomposition polys
-    h[0] = f2lin_rng_generic_init_zero();
     f2lin_rng_generic_copy(h[0], A[Q]);
     
     for (size_t i = 1; i < (1 << Q); ++i) {
@@ -169,7 +208,6 @@ void compute_decomposition_polys(const int Q, F2LinRngGeneric* h[1 << Q],
             ++set_bit;
         }
 
-        h[GRAY[i]] = f2lin_rng_generic_init_zero();
         f2lin_rng_generic_copy(h[GRAY[i]], h[GRAY[i - 1]]);
         f2lin_rng_generic_add(h[GRAY[i]], A[set_bit - 1]);
     }
@@ -198,7 +236,9 @@ F2LinRngGeneric* horner(F2LinRngGeneric* rng, const GF2X* jump_poly) {
     return rng;
 }
 
-static F2LinRngGeneric* horner_sliding_window(int Q, F2LinRngGeneric* rng, const GF2X* jump_poly, const F2LinRngGeneric* h[1 << Q]) {
+static 
+F2LinRngGeneric* sliding_window(int Q, F2LinRngGeneric* rng, const GF2X* jump_poly, 
+                                const F2LinRngGeneric* h[1 << Q]) {
     // use horners method with sliding window
     F2LinRngGeneric* tmp = f2lin_rng_generic_init_zero();
     int i = GF2X_deg(jump_poly); 
@@ -246,7 +286,10 @@ static F2LinRngGeneric* horner_sliding_window(int Q, F2LinRngGeneric* rng, const
     return rng;
 }
 
-static F2LinRngGeneric* horner_sliding_window_decomp(const int Q, F2LinRngGeneric* rng, const F2LinPolyDecomp* decomp_poly, const F2LinRngGeneric** h) {
+static 
+F2LinRngGeneric* sliding_window_decomp(const int Q, F2LinRngGeneric* rng, 
+                                       const F2LinPolyDecomp* decomp_poly, 
+                                       const F2LinRngGeneric** h) {
     F2LinRngGeneric* tmp = f2lin_rng_generic_init_zero();
 
     // go to first non zero coefficient
